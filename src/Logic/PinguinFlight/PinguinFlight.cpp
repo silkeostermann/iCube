d10f3c217f16c21fd44fefcdead95223472047fa
Andrea Cuttone <andreacuttone@gmail.com>
2012-03-19 11:56:20 +0100

    fixed penguins

--- src/Logic/PinguinFlight/PinguinFlight.cpp
+++ src/Logic/PinguinFlight/PinguinFlight.cpp
@@ -20,21 +20,34 @@
 
 PinguinFlight::PinguinFlight()
 {
-	this->image = new QImage ("./Logic/PinguinFlight/pinguin.png");
+	penguin0 = QImage("./Logic/PinguinFlight/pinguin0.png");
+	penguin90 = QImage("./Logic/PinguinFlight/pinguin90.png");
+	penguin180 = QImage("./Logic/PinguinFlight/pinguin180.png");
+	penguin270 = QImage("./Logic/PinguinFlight/pinguin270.png");
 }
 
 void PinguinFlight::ProcessSquares (const Square *recognizedSquares, int size)
 {
 	//printf ("[PENGUIN] Processing the squares\n");
 
-	Image images [size];
+	Image images [1];
 	for (int i=0; i < 1; i++)
 	{
+		QImage *img;
+		if(recognizedSquares[i].GetAngle() == 0) {
+			img = &penguin0;
+		}
+		else if(recognizedSquares[i].GetAngle() == 90) {
+			img = &penguin90;
+		}
+		else if(recognizedSquares[i].GetAngle() == 180) {
+			img = &penguin180;
+		}
+		else if(recognizedSquares[i].GetAngle() == 270) {
+			img = &penguin270;
+		}
 		CvPoint centCoord = recognizedSquares [i].GetCenterCoordinates ();
-		QTransform rot;
-		rot.rotate(recognizedSquares[i].GetAngle() - 90);
-    		QImage rotatedImage = image->transformed(rot);
-		images[i] = Image (rotatedImage, QPoint (centCoord.x, centCoord.y));
+		images[i] = Image (*img, QPoint (centCoord.x, centCoord.y));
 	}
 	
 	//printf ("[PENGUIN] %d pinguins found.\n", size);
@@ -49,7 +62,7 @@ void PinguinFlight::ProcessSquares (const Square *recognizedSquares, int size)
 
 PinguinFlight::~PinguinFlight ()
 {
-	delete image;
+
 }
 
 //void GetColor()
